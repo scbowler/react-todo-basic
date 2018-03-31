853c8b975985eb2dbe3b6ee8da8618be3cb94a78
Scott Bowler <scott.bowler@learningfuze.com>
2018-03-30 18:25:33 -0700

    Updated empty list message

--- src/components/list.js
+++ src/components/list.js
@@ -4,7 +4,7 @@ import ListItem from './list_item';
 export default props => {
 
     if(!props.list.length){
-        return <h2 className="center">No To Do Items</h2>
+        return <h2 className="center">No To Do Items, Add New Item Above</h2>
     }
 
     const listElements = props.list.map((item, index) => {
